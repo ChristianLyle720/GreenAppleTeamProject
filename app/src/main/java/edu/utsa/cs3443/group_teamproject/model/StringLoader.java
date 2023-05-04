b622db544aca6b4bb41884abfe836cc453d410ce
ChristianLyle720 <124601220+ChristianLyle720@users.noreply.github.com>
2023-05-03 20:40:42 -0500

    Update StringLoader.java

--- app/src/main/java/edu/utsa/cs3443/group_teamproject/model/StringLoader.java
+++ app/src/main/java/edu/utsa/cs3443/group_teamproject/model/StringLoader.java
@@ -3,7 +3,12 @@ package edu.utsa.cs3443.group_teamproject.model;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Scanner;
-
+/**
+* Static method to read from a file by using an InputStream and returning the contents of the file as an ArrayList of String arrays
+* Each String array in the ArrayList corresponds to a line in the file
+* @param fileName InputStream object for the file to be read from
+* @return ArrayList of String arrays containing contents of the read file
+*/
 public class StringLoader {
     public static ArrayList<String[]> loadData(InputStream inStream){
         ArrayList<String[]> list = new ArrayList<String[]>();
