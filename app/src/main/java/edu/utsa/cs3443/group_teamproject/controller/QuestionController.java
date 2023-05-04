f7a807a2bf686620321d9f0d9c98571806496109
ChristianLyle720 <124601220+ChristianLyle720@users.noreply.github.com>
2023-05-03 22:12:00 -0500

    Update QuestionController.java

--- app/src/main/java/edu/utsa/cs3443/group_teamproject/controller/QuestionController.java
+++ app/src/main/java/edu/utsa/cs3443/group_teamproject/controller/QuestionController.java
@@ -6,7 +6,10 @@ import android.widget.Toast;
 
 import edu.utsa.cs3443.group_teamproject.ChangeActivity;
 import edu.utsa.cs3443.group_teamproject.QuestionActivity;
-
+/**
+ * @author Green Apple Inc.
+ * QuestionController is a class responsible for handling interactions between SetActivity and the model classes.
+ */
 public class QuestionController implements View.OnClickListener {
     public static String name;
     private String user;
@@ -14,12 +17,27 @@ public class QuestionController implements View.OnClickListener {
     private String answer;
     private QuestionActivity questionActivity;
     private boolean changeScreen;
+    /**
+     * Constructor to create a new QuestionController
+     * @param name Name of the set of questions
+     * @param user Name of user of the app
+     * @param questionActivity The QuestionActivity instance
+     * @param changeScreen Boolean for if the QuestionController is meant to change screens
+     */
     public QuestionController(String name, String user, QuestionActivity questionActivity, boolean changeScreen){
         this.name = name;
         this.user = user;
         this.questionActivity = questionActivity;
         this.changeScreen = changeScreen;
     }
+    /**
+     * Constructor to create a new QuestionController
+     * @param name Name of the set of questions
+     * @param question A question being asked
+     * @param answer The answer to the question
+     * @param questionActivity The QuestionActivity instance
+     * @param changeScreen Boolean for if the QuestionController is meant to change screens
+     */
     public QuestionController(String name, String question, String answer, QuestionActivity questionActivity, boolean changeScreen){
         this.name = name;
         this.question = question;
@@ -27,7 +45,11 @@ public class QuestionController implements View.OnClickListener {
         this.questionActivity = questionActivity;
         this.changeScreen = changeScreen;
     }
-
+    /**
+     * Handles click events on the UI buttons in the ChangeActivity.
+     *  
+     * @param view The view that was clicked.
+     */
     public void onClick(View view){
         if(changeScreen){
             Intent intent = new Intent(questionActivity, ChangeActivity.class);
@@ -42,15 +64,25 @@ public class QuestionController implements View.OnClickListener {
             t.show();
         }
     }
-
+    /**
+    * Method to return a string representation of current set and associated user
+    * @return String representation of current set and associated user
+    */
     public String getSetText(){
         return user + "'s " + name + " set";
     }
-
+    /**
+     * Method to return a string representation of a question
+     * @param i The question number
+     * @return String representation of the question
+     */
     public String getQuestionText(int i){
         return (i+1) + ". " + question;
     }
-
+    /**
+     * Method to return a string representation of the answer to a question
+     * @return String representation of the answer to a question
+     */
     public String getAnswerText(){
         return answer;
     }
